838bfeba087409a344dad1af35569ed8e17828f3
Brian Jordan <bcjordan@gmail.com>
2012-04-21 14:16:15 -0400

    add asteroids and asteroid generator

--- level1.lua
+++ level1.lua
@@ -12,6 +12,7 @@ local scene = storyboard.newScene()
 -- include Corona's "physics" library
 local physics = require "physics"
 physics.start(); physics.pause()
+physics.setGravity(0,0)
 
 if(debug) then
     physics.setDrawMode( "hybrid" )
@@ -25,10 +26,13 @@ local mathlibapi = require("mathlib")
 local screenW, screenH = display.contentWidth, display.contentHeight
 local halfW, halfH = screenW * 0.5, screenH * 0.5
 local globeRadius = screenH * 0.25
-local globeX, globeY = halfW
+local globeX, globeY = halfW, halfH
 
 local globe = display.newImageRect("globe.jpg", 300, 300)
 
+local LEVEL_FILE = "level.txt"
+local ASTEROID_VELOCITY = 50
+local asteroids = {}
 
 -----------------------------------------------------------------------------------------
 -- BEGINNING OF YOUR IMPLEMENTATION
@@ -40,14 +44,17 @@ local globe = display.newImageRect("globe.jpg", 300, 300)
 
 -- Called when the scene's view does not exist:
 function scene:createScene( event )
-	local group = self.view
+    asteroids = scene:loadAsteroidsFile()
+    scene:createAsteroidsCallbacks() -- on game start...
+
+    local group = self.view
 
 	-- create a grey rectangle as the backdrop
     local background = display.newRect(0, 0, screenW, screenH)
     background:setFillColor(100, 125, 255, 255)
 
     globe.x, globe.y = halfW, halfH
-    physics.addBody(globe, { density = 1.0, friction = 0.3, bounce = 0.3, radius = globeRadius })
+    physics.addBody(globe, { density = 1.0, friction = 0.3, bounce = 0.5, radius = globeRadius })
     globe:addEventListener("touch", globe)
 
     -- Add center fake "mouse" joint to pin to center of screen.
@@ -59,20 +66,53 @@ function scene:createScene( event )
     group:insert( globe )
 end
 
-
-function globe:touch(event)
-end
-
-function loadAsteroidsFile( event )
-    local LEVEL_FILE = "myfile.txt"
-    local path = system.pathForFile( LEVEL_FILE, system.DocumentsDirectory )
+function scene:loadAsteroidsFile()
+    local path = system.pathForFile( LEVEL_FILE, system.ResourceDirectory )
     local file = io.open( path, "r" )
 
+    local waves = {}
     for line in file:lines() do
-        print( line )
+--        line:gsub(".", function(c) -- Perform a function on each character
+--            table.insert(wave, c)
+--        end)
+        table.insert(waves, ParseCSVLine(line, ','))
     end
-
     io.close( file )
+    return waves
+end
+
+function scene:createAsteroidsCallbacks()
+    local asteroidCreationDelay = 0 -- in ms
+
+    for index, wave in ipairs(asteroids) do
+        for index, value in ipairs(wave) do
+            if index == 1 then
+                asteroidCreationDelay = value
+            else
+                if(value == 'o') then
+                    local closure = function() return scene:addAsteroid( index * 90 ) end
+                    timer.performWithDelay(asteroidCreationDelay, closure)
+                end
+            end
+        end
+    end
+end
+
+function scene:addAsteroid(angle)
+    local point = {x = globeX + globeRadius + 100, y = globeY}
+    local center = {x = globeX, y = globeY }
+
+    local spawnLocation = rotateAboutPoint(point, center, angle, false)
+
+    local asteroid = display.newCircle(spawnLocation.x, spawnLocation.y, 10)
+    physics.addBody(asteroid, {radius = 10})
+
+    local velocityPair = rotatePoint({x= (-1) * ASTEROID_VELOCITY,y=0},angle)
+
+    asteroid:setLinearVelocity(velocityPair.x,velocityPair.y)
+
+    local group = self.view
+    group:insert(asteroid)
 end
 
 -- Called immediately after scene has moved onscreen:
@@ -118,6 +158,46 @@ function onScreenTouch(event)
         globe.touchJoint = physics.newJoint( "touch", globe, event.x, event.y )
         globe.touchJoint.maxForce = 4000
     end
+end
+
+function ParseCSVLine (line,sep)
+    local res = {}
+    local pos = 1
+    sep = sep or ','
+    while true do
+        local c = string.sub(line,pos,pos)
+        if (c == "") then break end
+        if (c == '"') then
+            -- quoted value (ignore separator within)
+            local txt = ""
+            repeat
+                local startp,endp = string.find(line,'^%b""',pos)
+                txt = txt..string.sub(line,startp+1,endp-1)
+                pos = endp + 1
+                c = string.sub(line,pos,pos)
+                if (c == '"') then txt = txt..'"' end
+                -- check first char AFTER quoted string, if it is another
+                -- quoted string without separator, then append it
+                -- this is the way to "escape" the quote char in a quote. example:
+                --   value1,"blub""blip""boing",value3  will result in blub"blip"boing  for the middle
+                until (c ~= '"')
+            table.insert(res,txt)
+            assert(c == sep or c == "")
+            pos = pos + 1
+        else
+            -- no quotes used, just look for the first separator
+            local startp,endp = string.find(line,sep,pos)
+            if (startp) then
+                table.insert(res,string.sub(line,pos,startp-1))
+                pos = endp + 1
+            else
+                -- no separator found -> use rest of string and terminate
+                table.insert(res,string.sub(line,pos))
+                break
+            end
+        end
+    end
+    return res
 end
 
 -----------------------------------------------------------------------------------------
